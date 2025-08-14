8486aa9c951c19ce91eaed20e7f4c0799fff1652
Puneet kumar Singh <punitku624@gmail.com>
2025-08-14 18:23:56 +0530

    Intial setup for the Project

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const Listing = require("./models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+   .then(()=>{
+    console.log("connected to DB");
+   })
+   .catch((err)=>{
+    console.log(err);
+   });
+
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req , res)=>{
+    res.send("Hii I am root");
+});
+
+app.get("/testListing", async (req, res)=>{
+    let sampleListing = new Listing({
+        title:"My New Villa",
+        descrption: "By the beach",
+        price:1200,
+        location:"Calangute, Goa",
+        country:"India",
+    });
+
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Successfull Testing");
+});
+
+app.listen(8080 , ()=>{
+    console.log("Server is Running at port 8080");
+});
+
+
+
